025e414932fb4a27f5cdf48fabdfbfcf28d3683d
lionDev <liosilman02@gmail.com>
2025-11-10 17:52:29 -0300

    vercel fix

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -6,6 +6,6 @@ export const CATEGORIES = [
 ]
 
 export const SHIPPING_COST = 1500
-export const BACKEND_URL = "mategambeta-backend.vercel.app/api"
+export const BACKEND_URL = import.meta.env.VITE_API_URL
 
 export const ADMIN_PASSWORD = "mate2024"
